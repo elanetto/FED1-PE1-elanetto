d290a2d0ea0cd2b87a8136941167bd340ad99b05
elanetto <101054274+elanetto@users.noreply.github.com>
2024-06-21 00:24:20 +0200

    Starting to create the javascript for the register page

---
+++ account/js/register.js
@@ -0,0 +1,12 @@
+// Make use of the register form and the API to register new user
+
+// set variables
+
+// input
+const username = document.getElementById('username');
+const registerEmail = document.getElementById('email-register');
+const registerPassword = document.getElementById('password-register');
+
+// button
+const registerButton = document.getElementById('register-btn');
+
