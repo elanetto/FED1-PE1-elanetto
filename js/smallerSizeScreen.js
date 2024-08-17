bc0cd1c8ad9bd2f519a796e9a930bc2386f76386
elanetto <101054274+elanetto@users.noreply.github.com>
2024-08-17 14:31:07 +0200

    Made carousel on index.html adjustable to smaller screen sizes

--- js/smallerSizeScreen.js
+++ js/smallerSizeScreen.js
@@ -1,10 +1,13 @@
 
 const dropdownButton = document.getElementById('dropdown-bars');
 const dropdownMenu = document.querySelector('.dropdown-content');
-const dropdownMenuItems = document.querySelectorAll('.dropdown-list-item');
 
 dropdownButton.addEventListener('click', function () {
-    dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
+    if (dropdownMenu.style.display === 'block'){
+        dropdownMenu.style.display = 'none';
+    }else {
+        dropdownMenu.style.display = 'block';
+    }
 });
 
 
