ed6ad6531f7f4e14d268b1e754d0c253e5971c4a
elanetto <101054274+elanetto@users.noreply.github.com>
2024-08-18 23:08:36 +0200

    Editing nav-links to work better when deployed

--- js/pagination.js
+++ js/pagination.js
@@ -80,7 +80,7 @@ document.addEventListener('DOMContentLoaded', function () {
             // Add click event listener to the entire post preview element to view the blog post
             postElement.querySelector('.blog-post-preview').addEventListener('click', function () {
                 localStorage.setItem('selectedPostId', cleanedPostId);
-                window.location.href = '../post/blogpost.html'; // Redirect to the blog post page
+                window.location.href = 'https://elanetto.github.io/FED1-PE1-elanetto/post/blogpost.html';
             });
 
             blogPostsContainer.appendChild(postElement);
